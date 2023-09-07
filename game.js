1e1b3b1dc1c131440d5f98d3ddc5893db0ed7d92
dev-vming <142391557+dev-vming@users.noreply.github.com>
2023-09-07 15:54:59 +0900

    Add files via upload

---
+++ game.js
@@ -0,0 +1,35 @@
+// 정답 
+const answer = 'apple';
+
+// 제출 버튼을 누르면 채점 진행
+const btn = document.querySelector('button');
+
+const t1 = document.querySelectorAll('.t1');
+const t2 = document.querySelectorAll('.t2');
+const t3 = document.querySelectorAll('.t3');
+const t4 = document.querySelectorAll('.t4');
+const t5 = document.querySelectorAll('.t5');
+
+const chances = [ t1, t2, t3, t4, t5 ];
+
+
+btn.addEventListener('click', function(){
+
+    chances.forEach((chance)=>{
+        for (let i=0; i<5; i++){
+            if(chance[i].value == answer[i]){
+                chance[i].style.backgroundColor = 'green';
+            } else if (chance[i].value == ''){
+                chance[i].style.backgroundColor = 'white';
+            } else if (answer.includes(chance[i].value)){
+                chance[i].style.backgroundColor = 'yellow';
+            }else {
+                chance[i].style.backgroundColor = 'lightgrey';
+            }
+        }
+    })
+
+    
+})
+
+
